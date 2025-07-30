38a4e58835ab963ef91e45607400fd3a036b99f5
alokraj10 <alokrajrnc629@gmail.com>
2025-07-30 23:18:54 +0530

    Updated Navbar.jsx and fixed image path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SCT_WD_01/',
-})
+  base: process.env.NODE_ENV === 'production' ? '/Store-Edge/' : '/',
+  server: {
+    port: 5173,
+    strictPort: true // Prevent port hopping
+  },
+  preview: {
+    port: 4173
+  }
+});
